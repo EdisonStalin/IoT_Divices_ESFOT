55f0ded4e4d4d11ae177d3d1d1e8f13cb8e0f0d2
EdisonStalin[C <edison.jumbo@epn.edu.ec>
2021-05-18 16:02:46 -0500

    V18.05.2021

--- libreria.py
+++ libreria.py
@@ -1,12 +1,14 @@
-class divice:
-    def __init__(self, Nombre, Direccion, Puerto):
+class Device:
+    def __init__(self, Nombre, Direccion, Puerto,status):
         self.Nombre = Nombre
         self.Direccion = Direccion
         self.Puerto = Puerto
+        self.status = status
 
     def toCollection(self):
         return{
             "Nombre": self.Nombre,
             "Direccion": self.Direccion,
-            "Puerto": self.Puerto
+            "Puerto": self.Puerto,
+            "Estado": self.status
         }
